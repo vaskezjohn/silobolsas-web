a1a4806b01458ee905194d383dbf8f6e77a047e3
fantito <crixalis@gmail.com>
2021-10-30 13:03:28 -0300

    campo filtro productor

--- src/app/silobolsas-component/campo/service/campo.service.ts
+++ src/app/silobolsas-component/campo/service/campo.service.ts
@@ -1,13 +1,19 @@
 import { Injectable} from '@angular/core';
 import { HttpHeaders, HttpClient } from '@angular/common/http';
 import { Campo } from '../models/campo.model';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
     providedIn: 'root'
 })
 export class CampoService {
     private httpOptions: any;
-    private basePath  = 'https://gendesoft.com/silobolsas-api/';
+
+
+    //private basePath  = 'https://gendesoft.com/silobolsas-api/';
+    private productorId = '08d999ca-09d7-49bf-8c0a-22601330e571';
+
+    private basePath  = environment.base_url;
 
     constructor(private http: HttpClient) {
         //Http Headers Options
@@ -19,7 +25,7 @@ export class CampoService {
     }
 
     CampoList() {
-      return this.http.get(this.basePath + 'odata/Campos', this.httpOptions);
+      return this.http.get(this.basePath + 'odata/Campos?%24filter=ProductoresID%20eq%20' + this.productorId , this.httpOptions);
     }
 
     add(campo: Campo) {
